e71e31f50655fd27a223c59b0fc9fc6146a2cd38
Gregorious4 <100717970+Gregorious4@users.noreply.github.com>
2023-10-26 16:20:13 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(800, 800);
+}
+function draw() {
+    background(220);
+    console.log("hi");
+}
+function draw() {
+    background(200);
+    textSize(32);
+    text('My javascript face', 280, 30);
+    ellipse(400, 800, 800, 400);
+    ellipse(400, 400, 400, 500);
+    circle(300, 300, 30);
+    circle(500, 300, 30);
+    point(300, 300);
+    point(500, 300);
+    triangle(300, 400, 400, 300, 500, 400);
+    rect(300, 500, 200, 10);
+    rect(100, 725, 350, 30);
+    text('Hello, my name is Greg', 100, 750);
+}
